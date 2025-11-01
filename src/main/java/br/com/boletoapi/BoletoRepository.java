19507a3369ed001ce128e6fc501a084ce982d563
Renato28 <renatonobrega87@gmail.com>
2025-11-01 20:40:02 -0300

    implementando o repositorio de boleto

---
+++ src/main/java/br/com/boletoapi/BoletoRepository.java
@@ -0,0 +1,10 @@
+package br.com.boletoapi;
+
+import br.com.boletoapi.model.Boleto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoletoRepository extends JpaRepository<Boleto, Long> {
+
+}
